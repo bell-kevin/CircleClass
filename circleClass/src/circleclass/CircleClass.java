da4fe7041293ca657ed04068842d1421bf3ba8f1
Kevin Bell <bellKevin@pm.me>
2022-06-01 13:03:07 -0600

    Update CircleClass.java

--- circleClass/src/circleclass/CircleClass.java
+++ circleClass/src/circleclass/CircleClass.java
@@ -1,8 +1,13 @@
 package circleclass;
 
+import java.util.Scanner;
+
 public class CircleClass {
 
     public static void main(String[] args) {
+        double radius;
+        System.out.println("Circle Class by Kevin Bell \n");
+        Scanner computerKeyboardInput = new Scanner(System.in);
 
     }
 
